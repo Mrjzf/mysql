1ea500effe045c728f7ae9d5b4ff4c082f50b9cd
Mrjzf <30620979+Mrjzf@users.noreply.github.com>
2017-08-21 21:48:03 +0800

    Delete 01-mysql.php

--- blog/01-mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header("Content-Type:text/html; charset=utf-8");
-    //链接数据库
-    $link = @mysql_connect('localhost','root','123456') or die('数据连接失败');
-
-    //选择数据库
-    mysql_select_db('blog',$link);   //mysql_query('use blog')
-
-    
